b02a16888761a87c558922606d0a816b7423b2c9
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-08 18:34:02 -0300

    tratamento de exceção no findById

--- src/main/java/com/lucas/projeto_estoque/services/UserService.java
+++ src/main/java/com/lucas/projeto_estoque/services/UserService.java
@@ -2,6 +2,7 @@ package com.lucas.projeto_estoque.services;
 
 import com.lucas.projeto_estoque.entities.User;
 import com.lucas.projeto_estoque.repositories.UserRepository;
+import com.lucas.projeto_estoque.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +23,6 @@ public class UserService {
     // busca um usuário pelo id
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 }
