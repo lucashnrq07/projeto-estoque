dd86894fe00a055469d3dbb96009cc9f56d5a3a4
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-08 18:53:19 -0300

    tratamento de exceção - delete

--- src/main/java/com/lucas/projeto_estoque/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/lucas/projeto_estoque/resources/exceptions/ResourceExceptionHandler.java
@@ -1,8 +1,9 @@
-package com.educandoweb.course.resources.exceptions;
+package com.lucas.projeto_estoque.resources.exceptions;
 
 import java.time.Instant;
 
 import com.lucas.projeto_estoque.resources.exceptions.StandardError;
+import com.lucas.projeto_estoque.services.exceptions.DatabaseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,6 +20,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
--- src/main/java/com/lucas/projeto_estoque/services/UserService.java
+++ src/main/java/com/lucas/projeto_estoque/services/UserService.java
@@ -1,13 +1,19 @@
-package com.lucas.projeto_estoque.services;
-
-import com.lucas.projeto_estoque.entities.User;
-import com.lucas.projeto_estoque.repositories.UserRepository;
-import com.lucas.projeto_estoque.services.exceptions.ResourceNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+package com.educandoweb.course.services;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.lucas.projeto_estoque.entities.User;
+import com.lucas.projeto_estoque.repositories.UserRepository;
+import com.lucas.projeto_estoque.services.exceptions.DatabaseException;
+import com.lucas.projeto_estoque.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -15,14 +21,42 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    // busca todos os usuários
     public List<User> findAll() {
         return repository.findAll();
     }
 
-    // busca um usuário pelo id
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public User update(Long id, User obj) {
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPassword(obj.getPassword());
     }
 }
