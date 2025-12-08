4aac55d093290652d448f008b070146aa9336e03
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-12-08 13:59:31 -0300

    primeiro commit

---
+++ src/test/java/com/lucas/projeto_estoque/ProjetoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.projeto_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
